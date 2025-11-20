f710478817752b127eab1d319489ce735fe07eb1
Timur Azimov <xasauzzz@users.noreply.github.com>
2025-11-20 10:54:28 +0500

    Enable media serving and upload product images

--- myshop/urls.py
+++ myshop/urls.py
@@ -10,11 +10,4 @@ urlpatterns = [
     path('', include(('shop.urls', 'shop'), namespace='shop')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-
-
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
